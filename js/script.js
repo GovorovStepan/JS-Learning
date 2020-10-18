01aaa5d8ba8bbbfd0839cea345a92b32ba22335d
Stepan <stepangovorov5911@gmail.com>
2020-10-18 13:22:50 +0300

    Второе практическое задание

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
-const personalMoiveDB = {
-          count : numberOfFilms,
-          movies :{},
-          actors :{},
-          generes : [''],
-          privat : false
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const personalMovieDB= {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    generes: [''],
+    privat: false
 
-      };
+};
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно слишком мало')
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+}
 
-const lastMovie1 = prompt("Последний просмотренный фильм?"),
-      lastMovieRate1 = prompt('На сколько оцените его ?'),
-      lastMovie2 = prompt("Последний просмотренный фильм?"),
-      lastMovieRate2 = prompt('На сколько оцените его ?');
+for (let i = 1; i < numberOfFilms; i++) {
+    const lastMovie = prompt('Последний просмотренный фильм?'),
+        lastMovieRate = prompt('На сколько оцените его ?');
+    if (lastMovie != null && lastMovieRate != null && lastMovie != '' && lastMovieRate != '' && lastMovie.length < 50) {
+        personalMovieDB.movies[lastMovie] = lastMovieRate;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 
-personalMoiveDB.movies[lastMovie1] = lastMovieRate1;
-personalMoiveDB.movies[lastMovie2] = lastMovieRate2;
+
+}
