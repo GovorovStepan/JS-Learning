5b39b02b5e2b3ac1551c90df31beeea4be76c297
Stepan <stepangovorov5911@gmail.com>
2020-10-19 18:24:04 +0300

    6(1) практическое задание

--- Пятое практическое задание/js/script.js
+++ Пятое практическое задание/js/script.js
@@ -1,17 +1,19 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
-
+5) Фильмы должны быть отсортированы по алфавиту */
 'use strict';
 
 const movieDB = {
@@ -23,24 +25,46 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
-//1
-document.querySelector('.promo__adv').remove(),
 
-    //2
-    document.querySelector('.promo__genre').remove(),
-    document.querySelector('.promo__bg').insertAdjacentHTML('afterbegin', '<div class="promo__genre">Драма</div>')
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list'),
+    addButton = document.querySelector('button');
 
-//3
-document.querySelector('.promo__bg').style.background = 'url("img/bg.jpg") center no-repeat ';
-document.querySelector('.promo__bg').style.backgroundSize = 'cover';
 
-//4
-document.querySelector('.promo__interactive-list').innerHTML = "";
+adv.forEach(item => {
+    item.remove();
+});
+
+
+addButton.addEventListener('click', (e) => {
+    e.preventDefault(),
+    movieDB.movies.push(document.querySelector('.adding__input').value),
+        movieList.innerHTML = "",
+        movieDB.movies.sort(),
+        movieDB.movies.forEach((film, i) => {
+            movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>`;
+        });
+});
+
+
+
+
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
 
 movieDB.movies.sort();
 
 movieDB.movies.forEach((film, i) => {
-    document.querySelector('.promo__interactive-list').innerHTML += `
+    movieList.innerHTML += `
         <li class="promo__interactive-item">${i + 1} ${film}
             <div class="delete"></div>
         </li>
